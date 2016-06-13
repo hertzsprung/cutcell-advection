2a373583bbb056be1de7b590d50bff4d26bc85aa
James Shaw <js102@zepler.net>
2016-06-13 03:38:26 +0000

    split into thermal and horizontal convergence

--- src/errors/collate-thermalAdvection.sh
+++ src/errors/collate-thermalAdvection.sh
@@ -2,7 +2,7 @@
 set -e
 make -C $ATMOSTESTS_DIR build/thermalAdvection-{slantedCell,btf}-{cubic,linear}Upwind-{24x10,32x15,48x20,72x30,96x40,144x60,192x80,288x120,384x160}/4000/l{2,inf}errorT.txt
 
-cat /dev/null > convergence.dat
+cat /dev/null > convergence-thermalAdvection.dat
 
 function testSet {
 cat \
@@ -14,32 +14,32 @@ cat \
   $ATMOSTESTS_DIR/build/thermalAdvection-btf-cubicUpwind-$1/4000/linferrorT.txt \
   $ATMOSTESTS_DIR/build/thermalAdvection-slantedCell-cubicUpwind-$1/4000/l2errorT.txt \
   $ATMOSTESTS_DIR/build/thermalAdvection-slantedCell-cubicUpwind-$1/4000/linferrorT.txt \
-| tr "\n" " " >> convergence.dat
+| tr "\n" " " >> convergence-thermalAdvection.dat
 }
 
-printf "24x10 833.33 " >> convergence.dat
+printf "24x10 833.33 " >> convergence-thermalAdvection.dat
 testSet 24x10
 
-printf "\n32x15 625 " >> convergence.dat
+printf "\n32x15 625 " >> convergence-thermalAdvection.dat
 testSet 32x15
 
-printf "\n48x20 416.66 " >> convergence.dat
+printf "\n48x20 416.66 " >> convergence-thermalAdvection.dat
 testSet 48x20
 
-printf "\n72x30 312.5 " >> convergence.dat
+printf "\n72x30 312.5 " >> convergence-thermalAdvection.dat
 testSet 72x30
 
-printf "\n96x40 208.33 " >> convergence.dat
+printf "\n96x40 208.33 " >> convergence-thermalAdvection.dat
 testSet 96x40
 
-printf "\n144x60 156.25 " >> convergence.dat
+printf "\n144x60 156.25 " >> convergence-thermalAdvection.dat
 testSet 144x60
 
-printf "\n192x80 104.16 " >> convergence.dat
+printf "\n192x80 104.16 " >> convergence-thermalAdvection.dat
 testSet 192x80
 
-printf "\n288x120 78.12 " >> convergence.dat
+printf "\n288x120 78.12 " >> convergence-thermalAdvection.dat
 testSet 288x120
 
-printf "\n384x160 52.08 " >> convergence.dat
+printf "\n384x160 52.08 " >> convergence-thermalAdvection.dat
 testSet 384x160
---
+++ src/errors/convergence-thermalAdvection.plt
@@ -0,0 +1,34 @@
+set terminal pdf size 6in,4in
+set output 'convergence-thermalAdvection.pdf'
+set logscale xy
+
+set xlabel "dx (m)"
+
+set style data linespoints
+
+set key outside top center
+
+set format y "10^{%L}"
+
+set multiplot layout 1,2
+
+set yrange [1e-7:1e-1]
+
+set title "l2"
+
+plot 'convergence-thermalAdvection.dat' using 2:3 lt 1 dt 1 pt 1 title 'btf linUp', \
+     'convergence-thermalAdvection.dat' using 2:5 lt 1 dt 2 pt 2 title 'slant linUp', \
+     'convergence-thermalAdvection.dat' using 2:7 lt 2 dt 1 pt 3 title 'btf cubUp', \
+     'convergence-thermalAdvection.dat' using 2:9 lt 2 dt 2 pt 4 title 'slant cubUp', \
+     x / 1e6 lt -1 dt 1 notitle, \
+     x**2 / 1e10 lt -1 dt 3 title '2nd order', \
+     x**3 / 1e13 lt -1 dt 4 title '3rd order'
+
+set title "linf"
+
+plot 'convergence-thermalAdvection.dat' using 2:4 lt 1 dt 1 pt 1 title 'btf linUp', \
+     'convergence-thermalAdvection.dat' using 2:6 lt 1 dt 2 pt 2 title 'slant linUp', \
+     'convergence-thermalAdvection.dat' using 2:8 lt 2 dt 1 pt 3 title 'btf cubUp', \
+     'convergence-thermalAdvection.dat' using 2:10 lt 2 dt 2 pt 4 title 'slant cubUp', \
+     x / 1e5 lt -1 dt 1 title '1nd order', \
+     x**2 / 5e8 lt -1 dt 3 title '2nd order'
